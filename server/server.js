560acd2cd6e8ca28794b7c5e50b9b4623cc21d31
Tomohiro Mitani <tmitani@tmitani-ltm2.internal.salesforce.com>
2018-12-19 20:39:20 +0900

    object change

--- server/server.js
+++ server/server.js
@@ -93,8 +93,11 @@ app.post('/logs', logplexMiddleware, (req, res) => {
   var loggedtime = logdrain.match(new RegExp(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))T[012]\d:[0-5]\d:[0-5]\d.\d{6}/g));
   var memory = String(value).match(new RegExp(/(?<=#memory_total=).*?(?=MB)/g));
   if (memory !==  null){
-    var primitiveMemory = memory.valueOf();
-    var obj = { log_time: loggedtime, total_memory: primitiveMemory };
+    var sLoggedtime = String(loggedtime[0]);
+    var memory_time = new Date(sLoggedtime);
+    var sMemory = String(value[0]);
+
+    var obj = { log_time: sMemory, total_memory: memory_time };
 //    console.log('Begining!');
     console.log(logdrain);
     console.log(JSON.stringify(obj));
