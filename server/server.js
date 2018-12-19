6c7f5f4701a6f08d23a5136b01940ecd80ce8f27
Tomohiro Mitani <tmitani@tmitani-ltm2.internal.salesforce.com>
2018-12-19 21:06:53 +0900

    removed unnecessary files

--- server/server.js
+++ server/server.js
@@ -19,46 +19,7 @@ app.use(basicAuth({
     realm: 'Imb4T3st4pp',
 }))
 
-
 let db;
-
-app.get('/api/issues', (req, res) => {
-  db.collection('issues').find().toArray()
-  .then(issues => {
-    const metadata = { total_count: issues.length };
-    res.json({ _metadata: metadata, records: issues });
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({ message: `Internal Server Error: ${error}` });
-  });
-});
-
-app.post('/api/issues', (req, res) => {
-  const newIssue = req.body;
-  newIssue.created = new Date();
-  if (!newIssue.status) {
-    newIssue.status = 'New';
-  }
-
-  const err = Issue.validateIssue(newIssue);
-  if (err) {
-    res.status(422).json({ message: `Invalid request: ${err}` });
-    return;
-  }
-
-  db.collection('issues').insertOne(Issue.cleanupIssue(newIssue)).then(result =>
-    db.collection('issues').find({ _id: result.insertedId }).limit(1)
-    .next()
-  )
-  .then(savedIssue => {
-    res.json(savedIssue);
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({ message: `Internal Server Error: ${error}` });
-  });
-});
 
 // Express allows arrays-of-middleware to act as a "single" middleware.
 var logplexMiddleware = [
